3b46e3f8beb43af5526d71284104058ef9a8b393
jameshu1205 <32165224+jameshu1205@users.noreply.github.com>
2018-04-14 15:28:35 +0800

    b681

---
+++ b681.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int point;
+  int day;
+  
+  while(cin >> point){
+    
+    if(point > 0){
+      day = point*2 - 1;
+  }else if(point < 0){
+      day = point*-2;
+  }
+  cout << day << "\n";
+}
+return 0;
+}
