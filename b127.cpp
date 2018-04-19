8485d9d87c8fb7c815a7fccf9d00dc855a241223
jameshu1205 <32165224+jameshu1205@users.noreply.github.com>
2018-04-19 18:36:42 +0800

    b127

--- b127.cpp
+++ b127.cpp
@@ -9,7 +9,7 @@ int main()
         times[0] = 1;
         times[1] = 1;
         
-        for(int j = 2;j <= i;j++){
+        for(int j = 2; j <= i; j++){
             times[j] = times[j - 1] + times[j - 2];
         }
         cout << times[i] << "\n";
